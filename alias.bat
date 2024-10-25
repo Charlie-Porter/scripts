f615be7afec405d9debc88fd483e266fc1c6a5c3
Charles porter <Charles.porter@ioausa.com>
2024-10-25 14:58:20 +0100

    eza

---
+++ alias.bat
@@ -0,0 +1,5 @@
+@echo off
+
+doskey n="C:\Program Files\Neovim\bin\nvim.exe" $*
+doskey l="C:\ProgramData\chocolatey\bin\lazygit.exe" $*
+doskey e="C:\Users\charles.porter\AppData\Local\Microsoft\WinGet\Packages\eza-community.eza_Microsoft.Winget.Source_8wekyb3d8bbwe\eza.exe"$*
